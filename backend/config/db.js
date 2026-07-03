a78c48012747a6d9b3901428609a04423797d910
fadlilatulmuna05-svg <fadlilatulmuna05@gmail.com>
2026-07-03 08:55:05 -0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,14 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error("❌ Gagal terhubung ke MySQL:");
-        console.error(err);
-        return;
-    }
-
-    console.log("✅ Berhasil terhubung ke MySQL");
-});
-
-module.exports = connection;
+module.exports = pool;
